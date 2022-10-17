c509471036432e70217f7a4c2f48e7fce58b14a0
TohidEq <TohidEq@gmail.com>
2022-10-17 13:01:22 +0330

    Create Styles and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Components:
+import Home from "./Components/Home/Home";
+import Search from "./Components/Search/Search";
+import Recipe from "./Components/Recipe/Recipe";
+import Create from "./Components/Create/Create";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipes/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Recipe/Recipe.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Recipe = (props: Props) => {
+  return (
+    <div>Recipe</div>
+  )
+}
+
+export default Recipe
