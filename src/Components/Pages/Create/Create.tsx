e8d5f3c680aff751df2671be5279a3688b7135f4
TohidEq <TohidEq@gmail.com>
2022-11-07 04:53:03 +0330

    20% Create page(useRef)

--- src/Components/Pages/Create/Create.tsx
+++ src/Components/Pages/Create/Create.tsx
@@ -1,31 +1,52 @@
-import React from "react";
+import React, { useRef } from "react";
 
 type Props = {};
 
 const Create = (props: Props) => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const methodRef = useRef<HTMLTextAreaElement>(null);
+  const timeRef = useRef<HTMLInputElement>(null);
+  const ingredientsRef = useRef<HTMLInputElement>(null);
   return (
     <div className="create">
       <h2 className="page-title">Create new Recipe</h2>
 
-      <form action="">
+      <form
+        action=""
+        onSubmit={() => {
+          console.log(11111);
+        }}
+      >
         <label htmlFor="">
           <span>Recipe Title:</span>
-          <input className="input" type="text" required />
+          <input ref={titleRef} className="input" type="text" required />
         </label>
 
         {/* ingredients go here */}
+        <label htmlFor="">
+          <span>Recipe ingredients:</span>
+          <div className="ingredients">
+            <input ref={ingredientsRef} type="text" className="input" />
+            <button className="btn">add</button>
+          </div>
+          <ul>
+            Current ingredients: <li>test</li> <li>test2</li>
+          </ul>
+        </label>
 
         <label htmlFor="">
           <span>Recipe Method:</span>
-          <textarea className="input" required></textarea>
+          <textarea ref={methodRef} className="input" required></textarea>
         </label>
 
         <label htmlFor="">
           <span>Cooking Time (minutes):</span>
-          <input className="input" type="number" />
+          <input ref={timeRef} className="input" type="number" required />
         </label>
 
-        <button className="btn">Submit</button>
+        <button className="btn" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
