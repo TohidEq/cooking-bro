346a7ba5b075dc7ee44adcfac5d47648f5c442ba
TohidEq <TohidEq@gmail.com>
2023-01-09 21:18:50 +0330

    Add mode(dark/light) to ThemeContext

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -2,6 +2,7 @@ import { createContext, useReducer } from "react";
 
 interface AppContextInterface {
   color: string;
+  mode: string;
   changeColor: (color: string) => void;
 }
 
@@ -11,21 +12,28 @@ const themeReducer = (state: any, action: any) => {
   switch (action.type) {
     case "CHANGE_COLOR":
       return { ...state, color: action.payload };
+
+    case "CHANGE_MODE":
+      return { ...state, mode: action.payload };
+
     default:
       return state;
   }
 };
 
 export function ThemeProvider({ children }: any) {
-  const [state, dispatch] = useReducer(themeReducer, { color: "#FF8303" });
+  const [state, dispatch] = useReducer(themeReducer, {
+    color: "#FF8303",
+    mode: "dark",
+  });
 
   const changeColor = (color: string) => {
     dispatch({ type: "CHANGE_COLOR", payload: color });
   };
 
-
-
-  
+  const changeMode = (mode: string) => {
+    dispatch({ type: "CHANGE_MODE", payload: mode });
+  };
   return (
     <ThemeContext.Provider value={{ ...state, changeColor }}>
       {children}
